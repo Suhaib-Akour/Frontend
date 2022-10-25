571d65ac782e9896cc87f112517339ef0bd72717
suhaibVs <sohayb.akour10@gmail.com>
2022-10-25 11:57:18 +0300

    javascript

---
+++ javascript/java.js
@@ -0,0 +1,7 @@
+onload=function(){
+document.getElementById("h3").style.backgroundColor="red";
+document.querySelector("h1").style.backgroundColor="blue"
+const color=document.getElementsByClassName("p");
+
+color[0].style.backgroundColor="black";
+}
